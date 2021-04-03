e9dd0a293b0f171ae8377ee0da4d54566a996ede
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2021-04-03 20:27:04 -0300

    Add Log In

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,33 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
+import mainLogo from '../../assets/logo.svg';
+import firebase from '../../config/firebase';
+import 'firebase/auth';
+
 
 function Login() {
+
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function logar() {
+
+        firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
+            alert("Usuário logado!");
+            setEmail("");
+            setSenha("");
+        }).catch(error => {
+            alert(error);
+        });
+
+
+
+    }
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+    function handlePasswordChange(e) {
+        setSenha(e.target.value);
+    }
+
+
     return (
 
         <div className="login-content dflex align-items-center">
             <form className="form-signin mx-auto">
-                <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+                <img class="mb-4" src={mainLogo} alt="" width="72" height="57" />
+                <h1 class="h3 mb-3 fw-normal text-white font-weight-bold">Login</h1>
 
                 <div class="form-floating mt-2 col-md-12">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                    <label for="floatingInput">Email address</label>
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" onChange={handleEmailChange} />
+                    <label for="floatingInput">Email</label>
                 </div>
                 <div class="form-floating mt-2 col-md-12">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                    <label for="floatingPassword">Password</label>
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Senha" onChange={handlePasswordChange} />
+                    <label for="floatingPassword">Senha</label>
                 </div>
 
-                <div class="checkbox mb-3 mt-2 col-md-12">
-                    <label>
-                        <input type="checkbox" value="remember-me" />
-            Remember me
-          </label>
-                </div>
-                <button class="w-100 btn btn-lg btn-login" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-            </form>
-        </div>
+                <button class="w-100 btn btn-lg btn-login mt-4" type="button" onClick={logar}> Sign in</button>
+
+
+            </form >
+
+            <div className="msg-login text-white text-center my-2">
+                <span><strong>Wow!</strong> você está conectado! &#128526;</span>
+                <span><strong>Ops!</strong> Verifique se usuário ou senha estão corretos!  &#128549;</span>
+            </div>
+
+            <div className="opcoes-login text-center  my-2">
+                <a href="#" className="mx-2"> Recuperar senha </a>
+                <span className="text-white"> &#9733;</span>
+                <a href="#" className="mx-2"> Quero senha </a>
+            </div>
+
+        </div >
     );
 }
 
