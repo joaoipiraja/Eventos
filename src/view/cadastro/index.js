f18b25f967307fe4350a025c652f781a2e7cefad
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2021-04-10 13:24:03 -0300

    Add cadastrar function

--- src/view/cadastro/index.js
+++ src/view/cadastro/index.js
@@ -1,18 +1,51 @@
-import React, { useState } from 'react';
+import React,{ useState } from 'react';
 import './cadastro.css';
 import 'firebase/auth';
+import { toast } from "react-toastify";
+import firebase from '../../config/firebase';
+import 'firebase/auth';
 
-function cadastro() {
+function Cadastro() {
+
+    const [email,setEmail] = useState();
+    const [senha,setSenha] = useState();
+
+    function cadastrar(){
+    
+        firebase.auth().createUserWithEmailAndPassword(email,senha).then(resultado => {
+            toast.success("Usuário cadastrado com sucesso");  
+        }).catch(error => {
+            console.log(error);
+            toast.error(error["message"]);
+        });
+
+        setEmail("");
+        setSenha("");
+
+    }
+    
     return(
-        <div className="form-cadastro">
+        <div className="cadastro-content dflex align-items-center">
+        
             <form className="text-center form-login mx-auto mt-5">
-                <h1 className="h3 mb-3 text-black font-weight-bold">Cadastro</h1>
-                <input type="email" className="form-control my-2" placeholder="Email"/>
-                <input type="password" className="form-control my-2" placeholder="Senha"/>
-                <button type="button" className="btn btn-lg btn-block mt-3 mb-55 btn-cadastro">Cadastrar</button>
+                <h1 className="h3 mb-3 text-black font-weight-bold text-white">Cadastro</h1>
+                <div class="form-floating mt-2 col-md-12">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" onChange={(e) => {setEmail(e.target.value)}} value={email}/>
+                    <label for="floatingInput">Email</label>
+                </div>
+                <div class="form-floating mt-2 col-md-12">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Senha" onChange={(e) => {setSenha(e.target.value)}} value={senha}/>
+                    <label for="floatingPassword">Senha</label>
+                </div>
+                <button type="button" className="w-100 btn btn-lg btn-cadastro mt-4" onClick={cadastrar}>Cadastrar</button>
             </form>
+
+
+                
+            
+     
         </div>
     );
 }
 
-export default cadastro;
+export default Cadastro;
